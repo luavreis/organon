ef1cfd4cba4a710face3499d4123543a6af362f5
lucasvreis <lvreis@usp.br>
2023-02-19 23:51:50 -0300

    Remove local test stuff

--- organon/app/Main.hs
+++ organon/app/Main.hs
@@ -3,12 +3,10 @@ module Main where
 import Data.Yaml qualified as Y
 import Site.Organon.Config qualified as Config
 import UnliftIO (catch)
-import UnliftIO.Directory
 import Site.Organon.Server (runOrganon)
 
 main :: IO ()
 main = do
-  setCurrentDirectory "/home/lucas/dados/projetos/sites/gatil"
   cfg <-
     Config.loadConfigWithDefault "organon.yaml"
       `catch` (error . toText . Y.prettyPrintParseException)
