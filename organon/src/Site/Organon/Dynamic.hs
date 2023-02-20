ea4da6ca11222f2f6f8e7e797650a4141fc94243
lucasvreis <lvreis@usp.br>
2023-02-20 19:37:53 -0300

    server: Use websocket instead of named pipe

--- organon/src/Site/Organon/Dynamic.hs
+++ organon/src/Site/Organon/Dynamic.hs
@@ -5,12 +5,14 @@ module Site.Organon.Dynamic (
   layoutDynamic,
   ondimDynamic,
   cacheDynamic,
+  wsConnDynamic,
 )
 where
 
-import Control.Monad.Logger (MonadLogger, logErrorNS)
+import Control.Monad.Logger
 import Data.Map (singleton)
 import Ema.Dynamic (Dynamic (..))
+import Network.WebSockets qualified as WS
 import Ondim.Targets.HTML.Load (loadTemplatesDynamic)
 import Org.Exporters.HTML (htmlTemplateDir)
 import Site.Org.Render.Types
@@ -20,7 +22,7 @@ import System.UnionMount qualified as UM
 import Text.XmlHtml qualified as X
 import UnliftIO (MonadUnliftIO)
 
-layoutDynamic :: (MonadUnliftIO m, MonadLogger m) => FilePath -> m (Dynamic m Layouts)
+layoutDynamic :: (MonadUnliftIO m, MonadLoggerIO m) => FilePath -> m (Dynamic m Layouts)
 layoutDynamic dir = do
   Dynamic
     <$> UM.mount dir [((), "**/*.html")] [] mempty \() fp _fa ->
@@ -30,7 +32,21 @@ layoutDynamic dir = do
           let name = fromString $ takeBaseName fp
           pure (singleton name tpl <>)
 
-ondimDynamic :: (MonadUnliftIO m, MonadLogger m) => FilePath -> m (Dynamic m OndimMS)
+ondimDynamic :: (MonadUnliftIO m, MonadLoggerIO m) => FilePath -> m (Dynamic m OndimMS)
 ondimDynamic dir = do
   ddir <- liftIO htmlTemplateDir
   Dynamic <$> loadTemplatesDynamic [dir, ddir]
+
+wsConnDynamic :: forall m. (MonadUnliftIO m, MonadLoggerIO m) => m (Dynamic m (IO ByteString))
+wsConnDynamic = do
+  value <- newEmptyTMVarIO
+  let getValue = atomically $ takeTMVar value
+      manage :: m ()
+      manage = do
+        liftIO $ WS.runServer "127.0.0.1" 9160 \pendingConn -> do
+          conn :: WS.Connection <- WS.acceptRequest pendingConn
+          WS.withPingThread conn 30 pass $
+            forever do
+              msg <- WS.receiveData conn
+              atomically $ putTMVar value msg
+  return $ Dynamic (getValue, const manage)
