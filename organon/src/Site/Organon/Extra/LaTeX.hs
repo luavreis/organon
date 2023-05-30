09a10a7c0e03bd6cd86d4a2c9ad36d5eabb04cc4
lucasvreis <lvreis@usp.br>
2023-05-29 23:57:18 -0300

    organon: remove outdated KaTeX code

--- organon/src/Site/Organon/Extra/LaTeX.hs
+++ organon/src/Site/Organon/Extra/LaTeX.hs
@@ -2,88 +2,17 @@
 
 module Site.Organon.Extra.LaTeX where
 
-import Control.Monad.Logger (MonadLogger, logErrorNS, logInfoNS)
+import Control.Monad.Logger (logInfoNS)
 import Data.Aeson (Result (..), fromJSON)
 import Data.Aeson.KeyMap qualified as KM
 import Data.ByteString.Base64 (encodeBase64)
-import Data.Map qualified as M
 import Data.Map qualified as Map
-import Data.Text qualified as T
-import Ondim.Extra (lookupAttr)
 import Ondim.Targets.HTML (HtmlNode)
-import Optics.Core ((%~))
-import Org.Exporters.Processing.OrgData (OrgData, keywords)
-import Org.Types
 import Site.Org.Render.Types
 import Site.Organon.Extra.LaTeX.Render
 import Site.Organon.Extra.LaTeX.Types
 import Site.Organon.Model (Model (..))
-import System.FilePath (takeDirectory, (-<.>), (</>))
-import UnliftIO (IOException, MonadUnliftIO, catch, modifyTVar)
-
-latexHeader :: OrgData -> Text
-latexHeader d =
-  case M.lookup "latex_header" (keywords d) of
-    Just (ValueKeyword t) -> t
-    _ -> ""
-
-getKaTeXPreamble ::
-  forall m.
-  (MonadUnliftIO m, MonadLogger m) =>
-  -- | Document's filepath
-  FilePath ->
-  OrgData ->
-  m Text
-getKaTeXPreamble fp datum = filteredPreamble
-  where
-    mathOp = "\\providecommand{\\DeclareMathOperator}[2]{\\providecommand{#1}{\\operatorname{#2}}}"
-
-    filteredPreamble :: m Text
-    filteredPreamble =
-      unlines . (mathOp :) . mapMaybe filt . concat
-        <$> mapM includeInput (lines $ latexHeader datum)
-
-    includeInput :: Text -> m [Text]
-    includeInput (T.strip -> txt)
-      | "\\input" `T.isPrefixOf` txt = do
-          let path = do
-                withNoPrefix <- T.stripPrefix "\\input{" txt
-                bare <- T.stripSuffix "}" $ T.stripEnd withNoPrefix
-                pure $ toString bare -<.> "tex"
-          file <-
-            mapM (readFileBS . (takeDirectory fp </>)) path
-              `catch` ( \(e :: IOException) -> do
-                          logErrorNS "Preamble Loading" (show e)
-                          pure Nothing
-                      )
-          return $ maybe [] (lines . decodeUtf8) file
-      | otherwise = return [txt]
-
-    filt :: Text -> Maybe Text
-    filt t =
-      if
-          | Just txt <- "\\newcommand" `T.stripPrefix` t ->
-              Just $ "\\providecommand" <> txt
-          | "\\providecommand"
-              `T.isPrefixOf` t
-              || "\\renewcommand"
-              `T.isPrefixOf` t
-              || "\\def"
-              `T.isPrefixOf` t
-              || "\\DeclareMathOperator"
-              `T.isPrefixOf` t ->
-              Just t
-          | otherwise -> Nothing
-
-processKaTeX ::
-  (MonadUnliftIO m, MonadLogger m) =>
-  -- | Document's filepath
-  FilePath ->
-  (OrgDocument, OrgData) ->
-  m (OrgDocument, OrgData)
-processKaTeX fp (doc, datum) = do
-  preambl <- getKaTeXPreamble fp datum
-  pure (doc & #documentProperties %~ M.insert "katex_preamble" preambl, datum)
+import UnliftIO (modifyTVar)
 
 -- | Encode Base64 data in link.
 makeDataURI :: (Text, ByteString) -> Text
