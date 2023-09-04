a5c71bd40532be78a582e928583ca78500f2bfce
lucasvreis <lvreis@usp.br>
2023-09-04 18:04:50 -0300

    match: use listExp

--- organon/src/Site/Organon/Extra/Regex.hs
+++ organon/src/Site/Organon/Extra/Regex.hs
@@ -1,6 +1,6 @@
 module Site.Organon.Extra.Regex where
 
-import Ondim.Extra (ifElse, lookupAttr')
+import Ondim.Extra.Expansions (ifElse, lookupAttr', listExp)
 import Site.Org.Render.Types
 import Text.Regex.PCRE
 import Text.Regex.PCRE.Text ()
@@ -13,6 +13,5 @@ regexExp node = do
     Just (getAllTextMatches -> matches) ->
       ifElse True node
         `binding` do
-          "match" #. forM_ (zip [0 ..] matches) \(i :: Int, m) -> do
-            show i #@ m
+          "match.result" #. listExp textData matches
     Nothing -> ifElse False node
